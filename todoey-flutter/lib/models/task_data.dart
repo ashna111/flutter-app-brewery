1f7f9110bcbff927b6ef2b76f310f703364cd466
ashna111 <ashnashah111@gmail.com>
2020-07-16 12:20:18 +0530

    todo application

---
+++ todoey-flutter/lib/models/task_data.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/foundation.dart';
+import 'package:todoey_flutter/models/task.dart';
+import 'dart:collection';
+
+class TaskData extends ChangeNotifier {
+  List<Task> _tasks = [
+    Task(name: 'Finish Full Stack Open 2020'),
+    Task(name: 'Make a portfolio website'),
+    Task(name: 'Start GATE'),
+    Task(name: 'Finish Flutter Bootcamp'),
+    Task(name: 'Learning how to learn course'),
+  ];
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  int get taskCount {
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTitle) {
+    final task = Task(name: newTaskTitle);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
+}
